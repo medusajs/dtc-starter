da6c7b142d639b77889e29c18d6800eea40c1691
Stevche Radevski <sradevski@live.com>
2025-12-18 15:22:51 +0100

    feat: Switch to using turbo

--- apps/storefront/src/modules/home/components/hero/index.tsx
+++ apps/storefront/src/modules/home/components/hero/index.tsx
@@ -1,5 +1,5 @@
-import { Github } from "@medusajs/icons"
-import { Button, Heading } from "@modules/common/components/ui"
+import { Github } from "@medusajs/icons";
+import { Button, Heading } from "@modules/common/components/ui";
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
@@ -18,10 +18,7 @@ const Hero = () => {
             Powered by Medusa and Next.js
           </Heading>
         </span>
-        <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
-          target="_blank"
-        >
+        <a href="https://github.com/medusajs/dtc-starter" target="_blank">
           <Button variant="secondary">
             View on GitHub
             <Github />
@@ -29,7 +26,7 @@ const Hero = () => {
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
